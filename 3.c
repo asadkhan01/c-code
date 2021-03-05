a47d70b62b6332b32530338fdb1987e1b49caca9
asadkhan01 <asadkhan01011995@gmail.com>
2021-03-05 06:39:30 -0800

    third file

---
+++ 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+int a;
+int b;
+int sum;
+printf("enter value of a : ");
+scanf("%D",&a);
+printf("enter vale of b :");
+scanf("%d",&b);
+sum=a+b;
+printf("sum of %d",sum);
+return 0;
+}
